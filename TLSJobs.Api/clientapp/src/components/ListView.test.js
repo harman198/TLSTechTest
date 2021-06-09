c35702b1bb8402a6bb56ff45973ac749c4c25f30
Harman Khangura <harman198@hotmail.com>
2021-06-09 23:22:08 +1000

    Added ListView component and added todo test to the test class along with it

---
+++ TLSJobs.Api/clientapp/src/components/ListView.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+describe("ListView", () => {
+    it.todo("Should Render 'No Rows Available' when rows passed in are empty");
+    it.todo("Should Render passed in rows As List View as default");
+    it.todo(
+        "Should Render passed in rows As Grid View when parameter passed in"
+    );
+});
