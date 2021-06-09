23409347115caeec8925ba471b9ac5ea9a92fd58
Harman Khangura <harman198@hotmail.com>
2021-06-09 20:59:33 +1000

    Added GetJobs Test with jobs in repository

--- TLSJobs.Api.Tests/Controllers/JobsControllerTests.cs
+++ TLSJobs.Api.Tests/Controllers/JobsControllerTests.cs
@@ -15,11 +15,12 @@ namespace TLSJobs.Api.Tests.Controllers
     public class JobsControllerTests
     {
 
+        private readonly Mock<IRepository> repository = new Mock<IRepository>();
+
         [Fact]
-        public void JobsController_ShouldReturnEmpty_WhenNoJobsAvailable()
+        public void GetJobs_ShouldReturnEmpty_WhenNoJobsAvailable()
         {
             var expectedResult = new List<Job>();
-            Mock<IRepository> repository  = new Mock<IRepository>();
             repository.Setup(x => x.GetJobs()).Returns(expectedResult);
 
             var controller = new JobsController(repository.Object);
@@ -27,6 +28,25 @@ namespace TLSJobs.Api.Tests.Controllers
             var actualResult = controller.GetJobs();
 
             actualResult.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public void GetJobs_ShouldReturnExpectedItems_WhenJobsAreAvailable()
+        {
+            var expectedResult = new List<Job>()
+            {
+                new Job() {Id = 1, Title = "Front End Developer", Description = "Expert Front End Developer needed for startup.", Type = JobType.frontend, Salary = 80000, AddedAt = DateTime.Now},
+                new Job() {Id = 2, Title = "Back End Developer", Description = "Expert Back End Developer needed for startup.", Type = JobType.frontend, Salary = 80000, AddedAt = DateTime.Now},
+                new Job() {Id = 3, Title = "Full Stack Developer", Description = "Expert Full Stack Developer needed for startup.", Type = JobType.frontend, Salary = 80000, AddedAt = DateTime.Now}
+            };
+            repository.Setup(x => x.GetJobs()).Returns(expectedResult);
+
+            var controller = new JobsController(repository.Object);
+
+            var actualResult = controller.GetJobs();
+
+            actualResult.Should().BeEquivalentTo(expectedResult);
+
         }
 
     }
