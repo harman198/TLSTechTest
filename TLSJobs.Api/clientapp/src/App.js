0b5a76ef23fa84f5abf1784133045fd1d993b795
Harman Khangura <harman198@hotmail.com>
2021-06-10 00:01:05 +1000

    Added ListView and GridView styles as required

--- TLSJobs.Api/clientapp/src/App.js
+++ TLSJobs.Api/clientapp/src/App.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import JobItem from "./components/JobItem";
+import ListView from "./components/ListView";
+import dummyJobs from "./fixtures/jobs.json";
+
 function App() {
-    return <div className="App">App</div>;
+    const [listStyle, setListStyle] = useState("list");
+
+    const buttonClickHandler = (ev) => {
+        if (listStyle === "list") {
+            setListStyle("grid");
+        } else {
+            setListStyle("list");
+        }
+        console.log("List Style", listStyle);
+    };
+    return (
+        <div className="App">
+            <button
+                onClick={buttonClickHandler}
+                style={{
+                    border: "1px solid gray",
+                    borderRadius: "6px",
+                    padding: "6px 12px",
+                    marginBottom: "40px",
+                    cursor: "pointer",
+                }}
+            >
+                Toggle List/Grid Style
+            </button>
+            <ListView listStyle={listStyle}>
+                {dummyJobs.map((job) => (
+                    <JobItem key={job.id} job={job} />
+                ))}
+            </ListView>
+        </div>
+    );
 }
 
 export default App;
--- TLSJobs.Api/clientapp/src/components/ListView.js
+++ TLSJobs.Api/clientapp/src/components/ListView.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import "./ListView.css";
+
 export default function ListView({ children, listStyle }) {
     const isListView =
         listStyle === undefined || listStyle === "" || listStyle === "list";
+    console.log(isListView);
     return (
-        <div className={"list " + isListView ? "" : "list--grid"}>
+        <div className={"list " + (isListView ? "" : "list--grid")}>
             {children}
         </div>
     );
